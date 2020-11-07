acfd5e8efda3ffd32b69849b7c3e3bb475c39782
Tony Collins <tjc@eltham-college.org.uk>
2020-11-07 12:25:44 +0000

    Converter added to convert string to string
    "0" to - "Red ""1"  "Green"

---
+++ WpfApp3/ColourCodeConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp3
+{
+    class ColourCodeConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object paramater, CultureInfo culture)
+        {
+            string v;
+            if ((string)value == "0")
+            {
+                v = "Red";
+            }
+            else v = "Blue";
+            return v;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
